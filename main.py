184ee58c5bd06f58ecc8dd64764977a2e4bdc4a1
itsgamerme8842 <93373390+itsgamerme8842@users.noreply.github.com>
2023-05-27 13:23:23 +0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+@namespace
+class StatusBarKind:
+    O2 = StatusBarKind.create()
+
+def on_b_pressed():
+    statusbar.value += 15
+controller.B.on_event(ControllerButtonEvent.PRESSED, on_b_pressed)
+
 def Jump_and_Gravity():
     scene.camera_follow_sprite(mySprite)
     mySprite.ay = 200
     if controller.A.is_pressed():
         if mySprite.is_hitting_tile(CollisionDirection.BOTTOM):
             mySprite.vy = -100
+            statusbar.value += -3
 def Turn_Right_Char():
     global Location, Location_y, mySprite, LR
     if LR == 1:
@@ -16,11 +25,34 @@ def Turn_Right_Char():
         mySprite.set_position(Location, Location_y)
         mySprite.set_stay_in_screen(True)
         controller.move_sprite(mySprite, 100, 0)
+        statusbar.attach_to_sprite(mySprite)
         LR = 0
 
 def on_left_pressed():
-    Turn_Left_Char()
+    Turn_left_Char()
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def Spawn_c02():
+    global mySprite2
+    mySprite2 = sprites.create(img("""
+            . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . . . . . . . . . . .
+        """),
+        SpriteKind.player)
 
 def on_right_pressed():
     Turn_Right_Char()
@@ -31,12 +63,7 @@ def on_menu_pressed():
     MENU = 1
 controller.menu.on_event(ControllerButtonEvent.PRESSED, on_menu_pressed)
 
-MENU = 0
-mySprite: Sprite = None
-LR = 0
-Location = 0
-Location_y = 0
-def Turn_Left_Char(menuvar: any = None):
+def Turn_left_Char():
     global Location, Location_y, mySprite, LR
     if LR == 0:
         Location = mySprite.x
@@ -46,9 +73,17 @@ def Turn_Left_Char(menuvar: any = None):
             myImage
         """), SpriteKind.player)
         mySprite.set_position(Location, Location_y)
-        controller.move_sprite(mySprite, 100, 0)
         mySprite.set_stay_in_screen(True)
+        controller.move_sprite(mySprite, 100, 0)
+        statusbar.attach_to_sprite(mySprite)
         LR = 1
+mySprite2: Sprite = None
+Location_y = 0
+Location = 0
+statusbar: StatusBarSprite = None
+LR = 0
+mySprite: Sprite = None
+MENU = 0
 MENU = 0
 mySprite = sprites.create(assets.image("""
     myImage
@@ -58,7 +93,18 @@ tiles.set_current_tilemap(tilemap("""
     level
 """))
 LR = 1
+statusbar = statusbars.create(20, 4, StatusBarKind.O2)
+statusbar.attach_to_sprite(mySprite)
+statusbar.set_label("O2")
+statusbar.set_status_bar_flag(StatusBarFlag.SMOOTH_TRANSITION, True)
 
 def on_forever():
     Jump_and_Gravity()
 forever(on_forever)
+
+def on_forever2():
+    statusbar.value += -1
+    pause(100)
+    if statusbar.value <= 0:
+        sprites.destroy(mySprite)
+forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+namespace StatusBarKind {
+    export const O2 = StatusBarKind.create()
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    statusbar.value += 15
+})
 function Jump_and_Gravity () {
     scene.cameraFollowSprite(mySprite)
     mySprite.ay = 200
     if (controller.A.isPressed()) {
         if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
             mySprite.vy = -100
+            statusbar.value += -3
         }
     }
 }
@@ -16,6 +23,7 @@ function Turn_Right_Char () {
         mySprite.setPosition(Location, Location_y)
         mySprite.setStayInScreen(true)
         controller.moveSprite(mySprite, 100, 0)
+        statusbar.attachToSprite(mySprite)
         LR = 0
     }
 }
@@ -37,11 +45,13 @@ function Turn_left_Char () {
         mySprite.setPosition(Location, Location_y)
         mySprite.setStayInScreen(true)
         controller.moveSprite(mySprite, 100, 0)
+        statusbar.attachToSprite(mySprite)
         LR = 1
     }
 }
 let Location_y = 0
 let Location = 0
+let statusbar: StatusBarSprite = null
 let LR = 0
 let mySprite: Sprite = null
 let MENU = 0
@@ -50,6 +60,17 @@ mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
 scene.setBackgroundColor(9)
 tiles.setCurrentTilemap(tilemap`level`)
 LR = 1
+statusbar = statusbars.create(20, 4, StatusBarKind.O2)
+statusbar.attachToSprite(mySprite)
+statusbar.setLabel("O2")
+statusbar.setStatusBarFlag(StatusBarFlag.SmoothTransition, true)
 forever(function () {
     Jump_and_Gravity()
+})
+forever(function () {
+    statusbar.value += -1
+    pause(100)
+    if (statusbar.value <= 0) {
+        sprites.destroy(mySprite)
+    }
 })
